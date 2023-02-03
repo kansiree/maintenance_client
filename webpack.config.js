23e39c55c7b3bef9e21599c06d985bf3012ee858
kansiree <kansiree_print@hotmail.com>
2023-02-03 11:20:09 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,9 @@
 module.exports = {
-    entry: "./src/js/main.js",
-    output: {
-        path:__dirname+ '/dist/',
-        filename: "bundle.js",
-        publicPath: '/'
-    },
-    devServer: {
-        inline: false,
-        contentBase: "./dist",
-    },
     module: {
         rules: [
-            {
-                test: /\.jsx?$/,
-                exclude:/(node_modules|bower_components)/,
-                rules: 'babel-loader',
-            }
+          { test: /\.css$/, use: 'css-loader' },
+          { test: /\.ts$/, use: 'ts-loader' }
         ]
-    }
+      }
 
 };
